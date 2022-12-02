77089575fa7efada6adac88eac7767dc77863bb4
desertyou <and18ste@gmail.com>
2022-12-02 09:26:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,126 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner fs = new Scanner(new File("input2.txt"));
+        Scanner baseTest= new Scanner("A Y\n" +
+            "B X\n" +
+            "C Z");
+        System.out.println(solve2_2(fs));
+    }
+
+    public static int solve1(Scanner sc) {
+        int mx1 = -1;
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            int val = 0;
+            while (!line.trim().equals("") && sc.hasNext()) {
+                val += Integer.parseInt(line);
+                line = sc.nextLine();
+            }
+            if (val > mx1) {
+                mx1 = val;
+            }
+        }
+        return mx1;
+    }
+
+    public static int solve1_2(Scanner sc) {
+        int mx1 = -1, mx2 = -1, mx3 = -1;
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            int val = 0;
+            while (!line.trim().equals("")) {
+                val += Integer.parseInt(line);
+                if (sc.hasNext())
+                    line = sc.nextLine();
+                else break;
+            }
+            if (val > mx1) {
+                mx3 = mx2;
+                mx2 = mx1;
+                mx1 = val;
+            } else if (val > mx2) {
+                mx3 = mx2;
+                mx2 = val;
+            } else if (val > mx3) {
+                mx3 = val;
+            }
+        }
+        return mx1 + mx2 + mx3;
+    }
+
+    public static int solve2_1(Scanner sc) {
+        int res = 0;
+        int i=0;
+        while (sc.hasNextLine()) {
+            i++;
+            String ln = sc.nextLine();
+            String[] rps = ln.split(" ");
+            int op=0, eu=0;
+
+            if (rps[0].trim().equals("A"))
+                op = 1;
+            else if (rps[0].trim().equals("B"))
+                op = 2;
+            else if(rps[0].trim().equals("C")) op = 3;
+
+            if (rps[1].trim().equals("X"))
+                eu = 1;
+            else if (rps[1].trim().equals("Y"))
+                eu = 2;
+            else if(rps[1].trim().equals("Z")) eu = 3;
+            if(eu==0 || op==0)
+            {
+                System.out.println(i);
+                System.out.println("wtf");
+            }
+            if(rps(op,eu)>0)
+                res+=6;
+            else if (rps(op,eu)==0)
+                res+=3;
+            res += eu;
+        }
+        return res;
+    }
+    public static int rps(int a, int b){
+        if( a == b)
+            return 0;
+        if((a==1 && b==3)||(a==2 && b==1)||(a==3 && b==2))
+            return -1;
+        return 1;
+    }
+    public static int solve2_2(Scanner sc) {
+        int res = 0;
+        int i=0;
+        while (sc.hasNextLine()) {
+            i++;
+            String ln = sc.nextLine();
+            String[] rps = ln.split(" ");
+            int op=0, eu=0;
+
+            if (rps[0].trim().equals("A"))
+                op = 1;
+            else if (rps[0].trim().equals("B"))
+                op = 2;
+            else if(rps[0].trim().equals("C")) op = 3;
+
+            if (rps[1].trim().equals("X")){
+                if(op>1)
+                    res+=(op-1);
+                else res+=3;
+            }
+            else if (rps[1].trim().equals("Y")){
+                res+=(op+3);
+            }
+            else if(rps[1].trim().equals("Z")) {
+                if(op<3)
+                    res+=(op+1+6);
+                else res+=(7);
+            }
+        }
+        return res;
+    }
+}
