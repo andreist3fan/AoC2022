b90bb381afe589f79f7db0a1e72af02099e6fe97
desertyou <and18ste@gmail.com>
2022-12-04 09:58:39 +0100

    added day 4 and prepared for day 5

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,27 @@ import java.util.Scanner;
 public class Main {
     /**
      * Where the magic happens
+     *
      * @param args args
      * @throws FileNotFoundException if file was not added yet
      */
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner fs = new Scanner(new File("input3.txt"));
-        Scanner baseTest= new Scanner("");
-        System.out.println(solve3_2(fs));
+        Scanner fs = new Scanner(new File("input4.txt"));
+        Scanner baseTest = new Scanner("""
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8""");
+        System.out.println(solve4_2(fs));
     }
 
 
     /**
      * For the first day of AoC 2022
      * INPUT FILE: input.txt
+     *
      * @param sc scanner
      * @return correct answers
      */
@@ -36,6 +44,7 @@ public class Main {
         }
         return mx1;
     }
+
     public static int solve1_2(Scanner sc) {
         int mx1 = -1, mx2 = -1, mx3 = -1;
         while (sc.hasNext()) {
@@ -64,44 +73,46 @@ public class Main {
     /**
      * For the second day of AoC 2022
      * INPUT FILE: input2.txt
+     *
      * @param sc scanner
      * @return correct answers
      */
     public static int solve2_1(Scanner sc) {
         int res = 0;
-        int i=0;
+        int i = 0;
         while (sc.hasNextLine()) {
             i++;
             String ln = sc.nextLine();
             String[] rps = ln.split(" ");
-            int op =(int) rps[0].charAt(0) - 'A'+1;
-            int eu =(int) rps[1].charAt(0) - 'X'+1;
-            if(eu==0 || op==0)
-            {
+            int op = (int) rps[0].charAt(0) - 'A' + 1;
+            int eu = (int) rps[1].charAt(0) - 'X' + 1;
+            if (eu == 0 || op == 0) {
                 System.out.println(i);
                 System.out.println("wtf");
             }
-            if(rps(op,eu)>0)
-                res+=6;
-            else if (rps(op,eu)==0)
-                res+=3;
+            if (rps(op, eu) > 0)
+                res += 6;
+            else if (rps(op, eu) == 0)
+                res += 3;
             res += eu;
         }
         return res;
     }
-    public static int rps(int a, int b){
-        if( a == b)
+
+    public static int rps(int a, int b) {
+        if (a == b)
             return 0;
-        if((a==1 && b==3)||(a==2 && b==1)||(a==3 && b==2))
+        if ((a == 1 && b == 3) || (a == 2 && b == 1) || (a == 3 && b == 2))
             return -1;
         return 1;
     }
+
     public static int solve2_2(Scanner sc) {
         int res = 0;
         while (sc.hasNextLine()) {
             String ln = sc.nextLine();
             String[] rps = ln.split(" ");
-            int op =(int) rps[0].charAt(0) - 'A'+1;
+            int op = (int) rps[0].charAt(0) - 'A' + 1;
 
             switch (rps[1].trim()) {
                 case "X":
@@ -125,51 +136,53 @@ public class Main {
     /**
      * For the third day of AoC 2022
      * INPUT FILE: input3.txt
+     *
      * @param sc scanner
      * @return correct answers
      */
-    public static int solve3_1(Scanner sc){
-        int res=0;
-        while(sc.hasNextLine()){
+    public static int solve3_1(Scanner sc) {
+        int res = 0;
+        while (sc.hasNextLine()) {
             String ln = sc.nextLine();
-            String half1 = ln.substring(0,ln.length()/2);
-            String half2 = ln.substring(ln.length()/2);
-            int i=0;
-            char x='0';
-            while(i<half1.length()){
-                x =half1.charAt(i);
-                if(half2.contains(Character.toString(x))){
-                   break;
-                }
-                i++;
-            }
-            if(Character.isLowerCase(x))
-                res=res + (int) x-'a'+1;
-            else if(Character.isUpperCase(x))
-                res=res + (int) x-'A'+27;
-            else System.out.println("wtf");
-        }
-        return res;
-    }
-    public static int solve3_2(Scanner sc){
-        int res=0;
-        while(sc.hasNextLine()){
-            String ln1 = sc.nextLine();
-            String ln2 = sc.nextLine();
-            String ln3=sc.nextLine();
-            int i=0;
-            char x='0';
-            while(i<ln1.length()){
-                x =ln1.charAt(i);
-                if(ln2.contains(Character.toString(x))&&ln3.contains(Character.toString(x))){
+            String half1 = ln.substring(0, ln.length() / 2);
+            String half2 = ln.substring(ln.length() / 2);
+            int i = 0;
+            char x = '0';
+            while (i < half1.length()) {
+                x = half1.charAt(i);
+                if (half2.contains(Character.toString(x))) {
                     break;
                 }
                 i++;
             }
-            if(Character.isLowerCase(x))
-                res=res + (int) x-'a'+1;
-            else if(Character.isUpperCase(x))
-                res=res + (int) x-'A'+27;
+            if (Character.isLowerCase(x))
+                res = res + (int) x - 'a' + 1;
+            else if (Character.isUpperCase(x))
+                res = res + (int) x - 'A' + 27;
+            else System.out.println("wtf");
+        }
+        return res;
+    }
+
+    public static int solve3_2(Scanner sc) {
+        int res = 0;
+        while (sc.hasNextLine()) {
+            String ln1 = sc.nextLine();
+            String ln2 = sc.nextLine();
+            String ln3 = sc.nextLine();
+            int i = 0;
+            char x = '0';
+            while (i < ln1.length()) {
+                x = ln1.charAt(i);
+                if (ln2.contains(Character.toString(x)) && ln3.contains(Character.toString(x))) {
+                    break;
+                }
+                i++;
+            }
+            if (Character.isLowerCase(x))
+                res = res + (int) x - 'a' + 1;
+            else if (Character.isUpperCase(x))
+                res = res + (int) x - 'A' + 27;
             else System.out.println("wtf");
         }
         return res;
@@ -178,13 +191,63 @@ public class Main {
     /**
      * For the fourth day of AoC 2022
      * INPUT FILE: input4.txt
+     *
      * @param sc scanner
      * @return correct answers
      */
-    public static int solve4_1(Scanner sc){
-        return -1;
+    public static int solve4_1(Scanner sc) {
+        int res = 0;
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] params = line.split("[, -]");
+            if ((Integer.parseInt(params[0]) <= Integer.parseInt(params[2]) &&
+                Integer.parseInt(params[1]) >= Integer.parseInt(params[3])) ||
+                Integer.parseInt(params[0]) >= Integer.parseInt(params[2]) &&
+                    Integer.parseInt(params[1]) <= Integer.parseInt(params[3]))
+                res++;
+        }
+        return res;
     }
-    public static int solve4_2(Scanner sc){
-        return 0;
+
+    public static int solve4_2(Scanner sc) {
+        int res = 0;
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] params = line.split("[, -]");
+            // 2 4    6 8
+            // 5 7    7 9
+            // 2 8    3 7
+            // 3 7    2 8
+            int a, b, c, d;
+            a = Integer.parseInt(params[0]);
+            b = Integer.parseInt(params[1]);
+            c = Integer.parseInt(params[2]);
+            d = Integer.parseInt(params[3]);
+            ///surely there's a better way to do this lmao
+            if ((c >= a && c <= b) || (d >= a && d <= b) ||
+                (a >= c && a <= d) || (b >= c && b <= d))
+                res++;
+
+        }
+        return res;
+    }
+
+    /**
+     * For the fifth day of AoC 2022
+     * INPUT FILE: input5.txt
+     *
+     * @param sc scanner
+     * @return correct answers
+     */
+    public static int solve5_1(Scanner sc) {
+        int res = 0;
+
+        return res;
+    }
+
+    public static int solve5_2(Scanner sc) {
+        int res = -1;
+
+        return res;
     }
 }
